920be544ed7a86e0dec37ee0a2e9e3de064e98bf
vibraniumforge <gregm9999@gmail.com>
2019-11-06 15:40:00 -0800

    Updates

---
+++ precache-manifest.522adb6bb6e0056d4b9057e1ec2e82e4.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1147c926fb4d9c0947dd2b70193afe13",
+    "url": "/video-poker/index.html"
+  },
+  {
+    "revision": "2cdcf2a8562a65e4f664",
+    "url": "/video-poker/static/css/main.bbaa4199.chunk.css"
+  },
+  {
+    "revision": "33a5575278658a8b9adf",
+    "url": "/video-poker/static/js/2.9ebf8801.chunk.js"
+  },
+  {
+    "revision": "2cdcf2a8562a65e4f664",
+    "url": "/video-poker/static/js/main.eb41ba89.chunk.js"
+  },
+  {
+    "revision": "f8131469b928d05a9658",
+    "url": "/video-poker/static/js/runtime~main.c71d1e10.js"
+  },
+  {
+    "revision": "7fb1f45b710e0d1ca186dd79c7d1113e",
+    "url": "/video-poker/static/media/10_of_clubs.7fb1f45b.png"
+  },
+  {
+    "revision": "d1be977d1098848d2f91996a9d630e20",
+    "url": "/video-poker/static/media/10_of_diamonds.d1be977d.png"
+  },
+  {
+    "revision": "4f45bcb3d89cfcb01905b915e697ce91",
+    "url": "/video-poker/static/media/10_of_hearts.4f45bcb3.png"
+  },
+  {
+    "revision": "624c5f7cc89fe2b9da0b3bf35b3e0858",
+    "url": "/video-poker/static/media/10_of_spades.624c5f7c.png"
+  },
+  {
+    "revision": "bb79bb6542ee263255d4c1cdcf7f9326",
+    "url": "/video-poker/static/media/6_of_diamonds.bb79bb65.png"
+  },
+  {
+    "revision": "dc88aca87d82dcf30f8f802839e46b44",
+    "url": "/video-poker/static/media/6_of_hearts.dc88aca8.png"
+  },
+  {
+    "revision": "4c293cdaab68eb4b240d570899f0853c",
+    "url": "/video-poker/static/media/7_of_diamonds.4c293cda.png"
+  },
+  {
+    "revision": "a8c6948d111a267ccb3b83c98461b312",
+    "url": "/video-poker/static/media/7_of_hearts.a8c6948d.png"
+  },
+  {
+    "revision": "63fcb3c204975ad08205996e2824c127",
+    "url": "/video-poker/static/media/8_of_clubs.63fcb3c2.png"
+  },
+  {
+    "revision": "82c8f08fdbb622af31f6c4e3cf10c4f0",
+    "url": "/video-poker/static/media/8_of_diamonds.82c8f08f.png"
+  },
+  {
+    "revision": "75da1f9567d3092e0fdce616f46bf289",
+    "url": "/video-poker/static/media/8_of_hearts.75da1f95.png"
+  },
+  {
+    "revision": "085e3ca3e6bea378c0f8a3fcaf6f5416",
+    "url": "/video-poker/static/media/8_of_spades.085e3ca3.png"
+  },
+  {
+    "revision": "73462a18ea651937ca04137797be99ac",
+    "url": "/video-poker/static/media/9_of_clubs.73462a18.png"
+  },
+  {
+    "revision": "6bc3ba1343500526ccc837651d3c963f",
+    "url": "/video-poker/static/media/9_of_diamonds.6bc3ba13.png"
+  },
+  {
+    "revision": "86d439cf96b38d90d9af95b429dcb509",
+    "url": "/video-poker/static/media/9_of_hearts.86d439cf.png"
+  },
+  {
+    "revision": "9a05178883f2308f91886fdd2ac87e40",
+    "url": "/video-poker/static/media/9_of_spades.9a051788.png"
+  },
+  {
+    "revision": "57e702715bb2b17fdb98490e2f207c78",
+    "url": "/video-poker/static/media/ace_of_clubs.57e70271.png"
+  },
+  {
+    "revision": "ce5bc5de8c7c718e2ae1698a9fe143a5",
+    "url": "/video-poker/static/media/ace_of_diamonds.ce5bc5de.png"
+  },
+  {
+    "revision": "1fcff170d8dae28c874e292a9f7f5e1c",
+    "url": "/video-poker/static/media/ace_of_hearts.1fcff170.png"
+  },
+  {
+    "revision": "9d2f7fb753d727c6c794991dac5589f4",
+    "url": "/video-poker/static/media/ace_of_spades.9d2f7fb7.png"
+  },
+  {
+    "revision": "e7051d9e94efc4a49e18e71fa5422d8d",
+    "url": "/video-poker/static/media/back@2x.e7051d9e.png"
+  },
+  {
+    "revision": "8e78fe87e9d3e05e070d0c937e67214c",
+    "url": "/video-poker/static/media/black_joker.8e78fe87.png"
+  },
+  {
+    "revision": "d6d61a82d0c1d41c2d9243aeb5090ad8",
+    "url": "/video-poker/static/media/jack_of_clubs.d6d61a82.png"
+  },
+  {
+    "revision": "f4f1c70bea52aa9b7633330730b8d720",
+    "url": "/video-poker/static/media/jack_of_diamonds.f4f1c70b.png"
+  },
+  {
+    "revision": "f634cf976686fc790a45ddf118c39610",
+    "url": "/video-poker/static/media/jack_of_hearts.f634cf97.png"
+  },
+  {
+    "revision": "43030464716ef1c7ab056eedabbfd914",
+    "url": "/video-poker/static/media/jack_of_spades.43030464.png"
+  },
+  {
+    "revision": "7c08604030c80100031a2c9a2d51058b",
+    "url": "/video-poker/static/media/king_of_clubs.7c086040.png"
+  },
+  {
+    "revision": "6547e008b50fc870bf7ab54b524a6f72",
+    "url": "/video-poker/static/media/king_of_diamonds.6547e008.png"
+  },
+  {
+    "revision": "a1e1243727b63c3aeb0bcc2cdd120c1f",
+    "url": "/video-poker/static/media/king_of_hearts.a1e12437.png"
+  },
+  {
+    "revision": "311032ee0effc346a912adad67d50e0e",
+    "url": "/video-poker/static/media/king_of_spades.311032ee.png"
+  },
+  {
+    "revision": "c8485ac99e53544715f62be0c2149a61",
+    "url": "/video-poker/static/media/queen_of_clubs.c8485ac9.png"
+  },
+  {
+    "revision": "06641f15288cb587249e4f2736908f42",
+    "url": "/video-poker/static/media/queen_of_diamonds.06641f15.png"
+  },
+  {
+    "revision": "349cb8fff708fd11615781973d29695c",
+    "url": "/video-poker/static/media/queen_of_hearts.349cb8ff.png"
+  },
+  {
+    "revision": "180d1f0d5d4f6aa019e589506c04f227",
+    "url": "/video-poker/static/media/queen_of_spades.180d1f0d.png"
+  },
+  {
+    "revision": "d55a4ada1cb2c5c393369a6c6d994e9b",
+    "url": "/video-poker/static/media/red_joker.d55a4ada.png"
+  }
+]);
