37b157c84c6c9f4def342b4146b7ef5832624d51
github username <gregm9999@gmail.com>
2019-11-29 19:19:54 -0800

    refactor

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,36 +1,5 @@
 import React from "react";
 
-// export default ({
-//   suit,
-//   icon,
-//   image,
-//   discard,
-//   discardToggle,
-//   isFirstDeal,
-//   card
-// }) => (
-//   // <div className={discard ? "card-selected" : "playing-card"}>
-//   <div>
-//     {/* <div className={discard ? "card-selected" : "card"}> */}
-//     {/* <h1 className={suit}>{icon}</h1>
-//     <h1 className={suit}>{image}</h1> */}
-//     <img
-//       src={require(`../playing-cards/${card.cardFront}`)}
-//       alt={`The ${card.image} of ${card.suit}`}
-//       className="playing-card"
-//     />
-//     <br />
-//     <button
-//       type="button"
-//       disabled={isFirstDeal}
-//       className={discard ? "discard-btn-selected" : "discard-btn"}
-//       onClick={discardToggle}
-//     >
-//       {discard ? "Keep" : "Discard"}
-//     </button>
-//   </div>
-// );
-
 const Card =({discard, discardToggle, isFirstDeal, card}) => {
   return ( <div>
     <img
@@ -38,7 +7,6 @@ const Card =({discard, discardToggle, isFirstDeal, card}) => {
         alt={`The ${card.name} of ${card.suit}`}
         className="playing-card"
       />
-      <br />
       <button
         type="button"
         disabled={isFirstDeal}
